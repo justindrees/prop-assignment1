d158fb165d9be23187fcc7b46b84cb4b187c31c3
Justin Drees <jzdrees@gmail.com>
2014-12-04 23:44:32 +0000

    implement circular inheritance prevention

--- prototype-based-multiple-inheritance.js
+++ prototype-based-multiple-inheritance.js
@@ -5,22 +5,18 @@ var myObject = {
             call: function(funcName, parameters) {
                 // Check if this object has a function whose name == funcName
                 if (this.hasOwnProperty(funcName)) {
-                    console.log("Note: at line 8, found the function");
                     return this[funcName].apply(this,parameters);
                 } else {
                     // Loop through prototypeList to check if this object has a parent with a function == funcName
                     for (var i = 0, ic = this.prototypeList.length; i < ic; i++) {
                         var parent = this.prototypeList[i];     // local variable for the object in the prototypeList
-                        console.log("Note: at line 14, parent index = " + i + ", object = " + parent);
 
                         if (parent.hasOwnProperty(funcName)) {  // check if parent has a function == funcName
-                            console.log("Note: at line 17");
                             i = ic; // stop outer loop by setting continuation condition to false
                             return parent[funcName].apply(parent,parameters);
                         } else if (parent.prototypeList.length > 0) {   // check if parent has parents in the prototypeList
                             for (var j = 0, jc = parent.prototypeList.length; j < jc; j++) {    // Loop through parent's parents in prototypeList
                                 var parentsParent = parent.prototypeList[j];    // local variable for the object in the prototypeList
-                                console.log("Note: at line 23, parentsParent = " + parentsParent);
                                 var isFuncInParent = parentsParent.call(funcName, parameters);   // use call method to recursively check if parentsParent (or any of its parents) has a function == funcName
                                 if (isFuncInParent != null) {
                                     i = ic; // stop outer loop by setting continuation condition to false
@@ -33,14 +29,17 @@ var myObject = {
                 }
             }
         };
-
         copy.prototype = myObject;
 
-        if (protoList == null) {
-            copy.prototypeList = [];
-        } else {
-            for (var i = 0, ic = protoList.length; i < ic; i++) {
-                copy.prototypeList.push(protoList[i]);
+        if (protoList == null) {        // if the protoList == null, then the object doesn't inherit any parents
+            copy.prototypeList = [];    // set the prototype's list of parents to an empty array
+        } else {                        // if the protoList == null, then the object does inherit parent(s)
+            for (var i = 0, ic = protoList.length; i < ic; i++) {   // loop through the array of parents
+                if (protoList[i].prototypeList.indexOf(copy) != -1) {   // if the parent has this object as a parent, then throw error
+                    throw "IllegalArgument";                            // but how can the parent have this object as a parent since this object is being created now?
+                } else {
+                    copy.prototypeList.push(protoList[i]);
+                }
             }
         }
         return copy;
@@ -57,7 +56,14 @@ var obj3 = myObject.create([obj1, obj2]);
 var result = obj3.call("func", ["hello"]);
 console.log("result: " + result);
 
-/* Notes:
-There can be name clashes (same name of prototype which another inherits).
-So use depth-first search from left to right and take the first that matches.
-*/
+// Circular Inheritance tests
+var objD = myObject.create();
+var objE = myObject.create(null);
+var objC = myObject.create([]);
+var objB = myObject.create([objC, objD, objE]);
+var objA = myObject.create([objB]);
+
+var objX = myObject.create(null);
+var objY = myObject.create([objX]);
+//objX.create([objY]);
+
